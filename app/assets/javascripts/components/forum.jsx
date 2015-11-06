936723891cb2c301e023388d4929dcac98b03326
rliusage <rui.liu@sage.com>
2015-11-06 17:03:36 -0500

    added comment_form component

--- app/assets/javascripts/components/forum.jsx
+++ app/assets/javascripts/components/forum.jsx
@@ -1,17 +1,7 @@
 var Forum = React.createClass({
   render: function(){
     return (
-      <form role="form">
-        <div className="form-group">
-          <label htmlFor="name">name:</label>
-          <input className="form-control" id="name"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="comment">Comment:</label>
-          <textarea className="form-control" rows="5" id="comment"></textarea>
-        </div>
-        <button type="submit" className="btn btn-default">Submit</button>
-      </form>
+      <CommentForm/>
     )
   }
 })
