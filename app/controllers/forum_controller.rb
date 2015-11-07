a8071eb0fbb23950b426c40629540e87a04963c7
rliusage <rui.liu@sage.com>
2015-11-06 23:26:32 -0500

    adde sanitization

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -2,12 +2,14 @@ class ForumController < ApplicationController
   def index
   end
   def load_messages
-    render json: Message.all
+    render json: Message.all.order(created_at: :asc)
   end
   def create_message
-    name = params[:name]
-    content = params[:content]
-    Message.create(name: name, content: content)
+    name = ActionController::Base.helpers.sanitize(params[:name])
+    content = ActionController::Base.helpers.sanitize(params[:content])
+
+    Message.create(name: name, content: content) if name.present? && content.present?
+
     render nothing: true
   end
 end
